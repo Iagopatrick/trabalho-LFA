545d5521edab71bf6db3e29644b55a7a2318b42b
Iagopatrick <iagogripp@hotmail.com>
2023-06-26 16:58:16 -0300

    início

---
+++ analisadorlexico.cpp
@@ -0,0 +1,118 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+/*      ANOTAÇÕES
+Talvez seja necessário analisar como uma string, pois a entrada 123456 só verifica se o primeiro elemento, '1', é um número.
+exemplo de entrada e saida:
+[2*(4+3)]
+ABRE COLCHETE
+NUM
+OP
+ABRE ASPAS
+NUM
+OP
+NUM
+FECHA ASPAS
+FECHA COLCHETE
+
+*/ 
+
+
+
+char ehNumero(char entrada){
+    // Função que verifica que a entrada é um token do tipo numero. A função faz isso usando o intervalo dos número em ASCII.
+
+    int cont = 0;
+    while(int(entrada) >= 48 && int(entrada) <= 57){
+        cont++;
+        cin >> entrada;
+    }
+    if(cont > 0){
+        cout << "<num>\n";
+    }
+
+    return entrada;
+}
+
+char ehVariavel(char entrada){
+    // Função que verifica que a entrada é um token do tipo variavel. Só para quando o próximo é uma opreção ou um externo.
+    // ATENÇÃO: falta verificar se é um externo
+
+    int cont = 0;
+    while(entrada != '+' && entrada != '-' && entrada != '/' && entrada != '*'){
+        cont++;
+        cin >> entrada;
+    }
+    if(cont > 0){
+        cout << "<variavel>\n";
+    }
+    return entrada;
+}
+
+void ehOp(char entrada){
+    // Função que verifica que a entrada é um token do tipo operação
+
+    if(entrada == '+' || entrada == '-' || entrada == '/' || entrada == '*'){
+        cout << "<OP>\n";
+    }
+
+}
+
+void externos(char entrada){
+    // CHAVES COLCHETES PARENTES
+    // Essa função irá verificar se o caracter é [] {} () e associar aos tokens. Faço isso verificando os intervalos,
+    // caso esteja no intervalo desses 'externos', então eu entro dentro da possibilidade de ser um token valido. Caso seja invalido,
+    // o programa para e aparece uma mensagem de erro.
+
+
+    if (int(entrada) >= 123 && int(entrada) <= 125){ //está no intervalo das chaves
+        if(entrada == '['){
+            cout << "<ABRE-CHAVES>\n";
+        }else if(entrada == ']'){
+            cout << "<FECHA-CHAVES>\n";
+        }else{
+            perror("Erro, caracter inválido!");
+        }
+    }
+    if (int(entrada) >= 91 && int(entrada) <= 93){ //está no intervalo dos colchetes
+
+    }
+
+
+
+}
+
+
+
+int main(){
+    char entrada;
+    char saida;
+    bool flag = true;
+    // tokens
+    // char num[10] = {0,1,2,3,4,5,6,7,8,9};
+    
+    do{
+        cin >> entrada;
+        cout << int(entrada) << " " << entrada;
+        if((entrada >= 32 && entrada<= 41) || (entrada >= 58 && entrada <= 64)){ //imtervalo de símbolos não aceitos na linguagem
+            flag = false;
+            cout << "entrada invalida!\n";
+        }else{
+            if(int(entrada) >= 48 && int(entrada) <= 57){
+                saida = ehNumero(entrada);
+            }else{
+                saida = ehVariavel(entrada);
+            }
+            ehOp(saida);
+        }
+        
+
+    }while(entrada != EOF);
+
+
+
+
+    return 0;
+}
---
+++ teste.c++
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main(){
+    char entrada;
+    // tokens
+    // char num[10] = {0,1,2,3,4,5,6,7,8,9};
+    cin >> entrada;
+    cout << int(entrada);
+
+
+
+
+    return 0;
+}
